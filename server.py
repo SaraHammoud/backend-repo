c17f4f318396f4391ab331ff43541ce4ed957991
SaraHammoud <sarahammoud.dev@gmail.com>
2024-07-08 01:10:20 +0300

    new way to connect ip address

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
+# import yaml
+
+# with open('config.yaml', 'r') as file:
+#     config = yaml.safe_load(file)
+
+# run_localhost = config['run_localhost']
+
+# class Server:
+#     def __init__(self):
+#         self.run_localhost = run_localhost
+
+#     def start(self):
+#         if self.run_localhost:
+#             print("Error: Running on localhost is not allowed.")
+#         else:
+#             print("Server starting...")
+
+# if __name__ == "__main__":
+#     server = Server()
+#     server.start()
 import yaml
+import os
 
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+config_file = os.path.join(os.path.dirname(__file__), 'C:/Users/user/Desktop/FSW/githubProjects/fullstack-repo/config.yaml')
 
-run_localhost = config['run_localhost']
+with open(config_file, 'r') as f:
+    config = yaml.safe_load(f)
 
-class Server:
-    def __init__(self):
-        self.run_localhost = run_localhost
+server_ip = config.get('ip_address')
 
-    def start(self):
-        if self.run_localhost:
-            print("Error: Running on localhost is not allowed.")
-        else:
-            print("Server starting...")
-
-if __name__ == "__main__":
-    server = Server()
-    server.start()
+print(f"Connecting to server at IP address: {server_ip}")
